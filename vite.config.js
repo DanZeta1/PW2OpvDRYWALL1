8dfccb6feb838504072fd0f2ccd1c100fa5b1067
Sadri29gb <sadrianromero29@gmail.com>
2026-04-11 23:43:46 -0600

    Fix: Headers duplicados Navegacion

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,28 @@ import purgecss from 'vite-plugin-purgecss'
 import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
-  base: '/PW2OPVDRYWALL1/',
+  base: process.env.NODE_ENV === 'production' ? '/PW2OPVDRYWALL1/' : '/',
 
   build: {
     cssMinify: true,
     rollupOptions: {
       input: {
-        main:                 'index.html',
-        acercaDe:             'html/acercaDe.html',
-        contactenos:          'html/contactenos.html',
-        formRegistro:         'html/formRegistro.html',
-        galeria:              'html/galeria.html',
-        nuestrosProgramas:    'html/nuestrosProgramas.html',
-        porqueOpv:            'html/porqueOpv.html',
-        preguntasFrecuentes:  'html/preguntasFrecuentes.html',
+        main: 'index.html',
+        acercaDe: 'html/acercaDe.html',
+        contactenos: 'html/contactenos.html',
+        formRegistro: 'html/formRegistro.html',
+        galeria: 'html/galeria.html',
+        nuestrosProgramas: 'html/nuestrosProgramas.html',
+        porqueOpv: 'html/porqueOpv.html',
+        preguntasFrecuentes: 'html/preguntasFrecuentes.html',
         terminosYcondiciones: 'html/terminosYcondiciones.html',
-        testimonios:          'html/testimonios.html',
+        testimonios: 'html/testimonios.html',
       }
     }
+  },
+
+  server: {
+    port: 5173,
   },
 
   plugins: [
